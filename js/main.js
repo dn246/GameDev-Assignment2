824cb4d8d045b90d86dbabcdc323a17651cb0c2a
Pasha/Paul Chorba <ninjadinosir@gmail.com>
2016-10-03 13:39:11 -0400

    basic setup stuff, just basic js and html files stolen from slides

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '',
+    { preload: preload, create: create, update: update });
+
+function preload() {
+    game.load.image('tag', 'assets/filename.png');
+    game.load.spritesheet('tag', 'assets/filename.png', width,
+        height);
+}
+
+function create() {
+// called once, when the game starts
+}
+
+function update() {
+// called every frame
+}
