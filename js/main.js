76a1de0a7bb8d0048efb706af6047d885af79e98
ghwlanglois <ghwlanglois@gmail.com>
2016-10-04 10:41:20 -0400

    added placeholder images

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import "Phaser";
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '',
+var game = new Phaser.Game(1334, 750, Phaser.AUTO, '',
     { preload: preload, create: create, update: update });
 
 function preload() {
