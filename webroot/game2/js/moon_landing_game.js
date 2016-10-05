e2f0d7ea4132bb68154c257eadb014143b617afa
Pasha/Paul Chorba <ninjadinosir@gmail.com>
2016-10-05 17:19:59 -0400

    things are happening, both good and bad, this might fix some?

--- webroot/game2/js/moon_landing_game.js
+++ webroot/game2/js/moon_landing_game.js
@@ -4,143 +4,148 @@ var game = new Phaser.Game(1334, 750, Phaser.AUTO, '',
     { preload: preload, create: create, update: update, render: render });
 
 var PLAYER_SPEED = 300;
-var PLAYER_START_X = 200;
-var PLAYER_START_Y = 400;
-
-//nope
-var TRASH_X_MIN = 300;
-var TRASH_X_RANGE = 1000;
-var TRASH_Y_MIN = 320;
-var TRASH_Y_RANGE = 200;
+var PLAYER_START_X = 670;
+var PLAYER_START_Y = 320;
+var PLAYER_MAX_X = 1275;
+var PLAYER_MAX_Y = 650;
+var PLAYER_MIN_X = 65;
+var PLAYER_MIN_Y = 130;
 
 function preload() {
     game.load.image('moon_background', 'assets/images/moon_landing_background.png');
     game.load.image('moon_set', 'assets/images/moon_landing_set.png');
-    //nope
-    game.load.image('trash', 'assets/images/9_11_trash.png');
-    game.load.spritesheet('player_walking', 'assets/images/cameraman_walk_cycle.png', 100,
-        100,4);
+    game.load.spritesheet('player_walk', 'assets/images/main_character_walk_cycle.png', 114,
+        204, 4, 0, 235);
+    game.load.spritesheet('cameraMan_walk', 'assets/images/cameraman_walk_cycle.png', 100,
+        100, 4);
+    game.load.spritesheet('director_walk', 'assets/images/director_walk_cycle.png', 114,
+        226, 4, 0, 235);
+    game.load.spritesheet('janitor_walk', 'assets/images/janitor_walk_cycle.png', 100,
+        100, 4);
 }
 
-// Object declarations
-var backgrounds;
-var moon_set;
 var player;
-var trash;
-var cursors;
+var cameraMan;
+var director;
+var janitor;
+var moon_set;
+var cursor;
 var scoreText;
 var timeText;
 var timer;
+var tween;
 
-// Variable declarations
 var fading = false;
 var score = 0;
 var time_left = 90;
-var seamless_total = 1;
+var potCups = 5;
+
+
 
 function create() {
-    game.world.setBounds(0, 0, 1334*(seamless_total+1), 750);
+    game.world.setBounds(0, 0, 1334, 750);
 
     // Add the group of moon_set to the game
     moon_set = game.add.group();
     moon_set.create(0,0,'moon_set');
 
     // Set up player sprite and animation
-    player = game.add.sprite (PLAYER_START_X,PLAYER_START_Y,'player_walking');
-    player.animations.add('player_walking', [1,2,3,4], 6, true);
+    player = game.add.sprite (PLAYER_START_X,PLAYER_START_Y,'player_walk');
+    player.animations.add('player_walking', [0,1,2,3], 6, true);
+    player.animations.add('player_idle', [0], 6, true);
     player.anchor.setTo(0.5, 0.5);
+    game.input.onDown.add(movePlayer, this);
 
-    // Add the group of trash bits to the game
-    trash = game.add.group();
-    generateTrash();
+    cameraMan = game.add.sprite (PLAYER_START_X+200,PLAYER_START_Y,'cameraMan_walk');
+    cameraMan.animations.add('cameraMan_walking', [0,1,2,3], 6, true);
+    cameraMan.animations.add('cameraMan_idle', [0], 6, true);
+
+    director = game.add.sprite (PLAYER_START_X-200,PLAYER_START_Y,'director_walk');
+    director.animations.add('director_walking', [0,1,2,3], 6, true);
+    director.animations.add('director_idle', [0], 6, true);
+
+    janitor = game.add.sprite (PLAYER_START_X,PLAYER_START_Y+200,'janitor_walk');
+    janitor.animations.add('janitor_walking', [0,1,2,3], 6, true);
+    janitor.animations.add('janitor_idle', [0], 6, true);
 
     // Set up text box for timer and score variable in UI
     var timeStyle = { font: "24px Arial", fill: "#000000", align: "left"};
-    timeText = game.add.text(game.camera.x+25, game.camera.y+25, 'Time Rem: 90', timeStyle);
+    timeText = game.add.text(game.camera.x+25, game.camera.height-50, 'Time Rem: 90', timeStyle);
     var scoreStyle = { font: "24px Arial", fill: "#000000", align: "right"};
-    scoreText = game.add.text(game.camera.x+game.camera.width-140, game.camera.y+25, 'Score: 0', scoreStyle);
+    scoreText = game.add.text(game.camera.x+game.camera.width-140, game.camera.height-50, 'Score: 0', scoreStyle);
 
     // Set up game physics, keyboard input, camera fade listener
     game.physics.arcade.enable(player);
-    cursors = game.input.keyboard.createCursorKeys();
+    cursor = game.input.keyboard.createCursorKeys();
     game.camera.onFadeComplete.add(resetFade, this);
+
+
 
     // Start the timer for the level
     game.time.events.add(Phaser.Timer.SECOND, secondTick, this);
 }
 
 function update() {
-    // Set the interpolating camera to follow the player
-    //game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.05, 0.05);
-    // Add collision to trash objects so they can be picked up
-    game.physics.arcade.enable(trash);
-    game.physics.arcade.overlap(player, trash, collectTrash, null, this);
+
 
     updateUI();
-    playerMovement();
-    checkEndlessGeneration();
 }
 
 function render() {
-    //game.debug.text( seamless_total.toString(), 100, 380 );
+    game.debug.pointer(game.input.mousePointer);
 }
 
-function playerMovement() {
-    // Check input for arrow keys to move player with animation
-    player.body.velocity.x = 0;
-    player.body.velocity.y = 0;
-
-    if (!fading) {
-        if (cursors.left.isDown && player.body.x > 0) {
-            player.body.velocity.x = -PLAYER_SPEED;
-            player.animations.play('player_walking');
-            player.scale.x = -1;
-        } else if (cursors.right.isDown && player.body.x) {
-            player.body.velocity.x = PLAYER_SPEED;
-            player.animations.play('player_walking');
-            player.scale.x = 1;
-        }
-
-        if (cursors.up.isDown && player.body.y > 200) {
-            player.body.velocity.y = -PLAYER_SPEED;
-            player.animations.play('player_walking');
-        } else if (cursors.down.isDown && player.body.y < 650) {
-            player.body.velocity.y = PLAYER_SPEED;
-            player.animations.play('player_walking');
-        }
+function movePlayer (pointer) {
+    // Cancel any movement that is currently happening
+    if (tween && tween.isRunning) {
+        tween.stop();
     }
-}
 
-function checkEndlessGeneration() {
-    if (player.body.x > seamless_total * 1334) {
-        seamless_total++;
-        backgrounds.create(1334*seamless_total,0,'9_11_background');
-        moon_set.create(1334*seamless_total,200,'9_11_table');
-        game.world.setBounds(0, 0, 1334*(seamless_total+1), 750);
-        generateTrash();
+    // Flip the sprite and start the walking animation
+    if (game.input.worldX >= player.body.x) {
+        player.scale.x = 1;
+    } else {
+        player.scale.x = -1;
     }
-}
+    player.animations.play('player_walking', true);
 
-function generateTrash() {
-    for (i = 0; i < Math.floor((Math.random() * 5) + 5); i++) {
-        trash.create((seamless_total-1)*1334+Math.floor((Math.random() * TRASH_X_RANGE) + TRASH_X_MIN), Math.floor((Math.random() * TRASH_Y_RANGE) + TRASH_Y_MIN),'trash');
+    // Determine the time it will take to get to the pointer
+    var duration = (game.physics.arcade.distanceToPointer(player, pointer) / PLAYER_SPEED) * 1000;
+    // Start tween movement towards pointer
+    //570-760x,170-220y is coffee table range
+    //66-1250?,130-650y window size
+    var tempX = game.input.worldX;
+    var tempY = game.input.worldY;
+    if (tempX < PLAYER_MIN_X){
+        tempX = PLAYER_MIN_X;
     }
+    else if(tempX > PLAYER_MAX_X){
+        tempX = PLAYER_MAX_X;
+    }
+    if (tempY < PLAYER_MIN_Y){
+        tempY = PLAYER_MIN_Y;
+    }
+    else if(tempY > PLAYER_MAX_Y){
+        tempY = PLAYER_MAX_Y;
+    }
+    tween = game.add.tween(player).to({ x:tempX, y:tempY }, duration, Phaser.Easing.Linear.None, true);
+
+    // Set a timer to stop the animation
+    game.time.events.add(duration, function() {
+        player.animations.stop('player_walking');
+        player.animations.play('player_idle');}, this);
 }
 
 function updateUI() {
     // Update the text position as the camera moves
-    timeText.x = game.camera.x+25;
-    timeText.y = game.camera.y+25;
-    scoreText.x = game.camera.x+game.camera.width-140;
-    scoreText.y = game.camera.y+25;
+    timeText.x = game.camera.x+game.camera.width-170;
+    timeText.y = game.camera.height-100;
+    scoreText.x = game.camera.x+game.camera.width-170;
+    scoreText.y = game.camera.height-50;
 }
 
-function collectTrash(player, t) {
-    // Remove the trash from the screen
-    t.kill();
-    // Update score and scoreText
-    score += 10;
-    scoreText.text = 'Score: ' + score;
+function refillPot(){
+
 }
 
 function secondTick() {
@@ -186,8 +191,3 @@ function resetFade() {
     fading = false;
 }
 
-function checkOverlap(spriteA, spriteB) {
-    var boundsA = spriteA.getBounds();
-    var boundsB = spriteB.getBounds();
-    return Phaser.Rectangle.intersects(boundsA, boundsB);
-}
