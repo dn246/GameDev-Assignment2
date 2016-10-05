45276a052016c5201eb9d6a6120db1a2b9807a88
Pasha/Paul Chorba <ninjadinosir@gmail.com>
2016-10-05 13:49:01 -0400

    stuff is working a little better now...

--- webroot/game2/js/moon_landing_game.js
+++ webroot/game2/js/moon_landing_game.js
@@ -1,27 +1,30 @@
-
-
+// MiniGame for moonLanding conspiracy, basically DinerDash...
 
 var game = new Phaser.Game(1334, 750, Phaser.AUTO, '',
     { preload: preload, create: create, update: update, render: render });
 
 var PLAYER_SPEED = 300;
-var PLAYER_START_X = 100;
-var PLAYER_START_Y = 410;
+var PLAYER_START_X = 200;
+var PLAYER_START_Y = 400;
+
+//nope
 var TRASH_X_MIN = 300;
 var TRASH_X_RANGE = 1000;
 var TRASH_Y_MIN = 320;
 var TRASH_Y_RANGE = 200;
 
 function preload() {
-    game.load.image('moon_landing_background', 'assets/images/moon_landing_background.png');
+    game.load.image('moon_background', 'assets/images/moon_landing_background.png');
     game.load.image('moon_set', 'assets/images/moon_landing_set.png');
+    //nope
     game.load.image('trash', 'assets/images/9_11_trash.png');
-    game.load.spritesheet('player_walk', 'assets/images/cameraman_walk_cycle.png', 100,100,5);
+    game.load.spritesheet('player_walking', 'assets/images/cameraman_walk_cycle.png', 100,
+        100,4);
 }
 
 // Object declarations
 var backgrounds;
-var tables;
+var moon_set;
 var player;
 var trash;
 var cursors;
@@ -32,26 +35,19 @@ var timer;
 // Variable declarations
 var fading = false;
 var score = 0;
-var time_left = 30;
+var time_left = 90;
 var seamless_total = 1;
 
 function create() {
     game.world.setBounds(0, 0, 1334*(seamless_total+1), 750);
 
-    // Add the group of backgrounds to the game
-    backgrounds = game.add.group();
-    backgrounds.create(0,0,'moon_landing_background');
-    backgrounds.create(1334,0,'moon_landing_background');
-
-    // Add the group of tables to the game
-    tables = game.add.group();
-    tables.create(0,200,'moon_set');
-    tables.create(1334,200,'moon_set');
+    // Add the group of moon_set to the game
+    moon_set = game.add.group();
+    moon_set.create(0,0,'moon_set');
 
     // Set up player sprite and animation
-    player = game.add.sprite (PLAYER_START_X,PLAYER_START_Y,'player_walk');
-    player.animations.add('walk');
-    player.animation.play('walk', 5, true);
+    player = game.add.sprite (PLAYER_START_X,PLAYER_START_Y,'player_walking');
+    player.animations.add('player_walking', [1,2,3,4], 6, true);
     player.anchor.setTo(0.5, 0.5);
 
     // Add the group of trash bits to the game
@@ -60,7 +56,7 @@ function create() {
 
     // Set up text box for timer and score variable in UI
     var timeStyle = { font: "24px Arial", fill: "#000000", align: "left"};
-    timeText = game.add.text(game.camera.x+25, game.camera.y+25, 'Time Rem: 30', timeStyle);
+    timeText = game.add.text(game.camera.x+25, game.camera.y+25, 'Time Rem: 90', timeStyle);
     var scoreStyle = { font: "24px Arial", fill: "#000000", align: "right"};
     scoreText = game.add.text(game.camera.x+game.camera.width-140, game.camera.y+25, 'Score: 0', scoreStyle);
 
@@ -75,7 +71,7 @@ function create() {
 
 function update() {
     // Set the interpolating camera to follow the player
-    game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.05, 0.05);
+    //game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.05, 0.05);
     // Add collision to trash objects so they can be picked up
     game.physics.arcade.enable(trash);
     game.physics.arcade.overlap(player, trash, collectTrash, null, this);
@@ -97,20 +93,20 @@ function playerMovement() {
     if (!fading) {
         if (cursors.left.isDown && player.body.x > 0) {
             player.body.velocity.x = -PLAYER_SPEED;
-            player.animations.play('player_crawling');
+            player.animations.play('player_walking');
             player.scale.x = -1;
         } else if (cursors.right.isDown && player.body.x) {
             player.body.velocity.x = PLAYER_SPEED;
-            player.animations.play('player_crawling');
+            player.animations.play('player_walking');
             player.scale.x = 1;
         }
 
-        if (cursors.up.isDown && player.body.y > 253) {
+        if (cursors.up.isDown && player.body.y > 200) {
             player.body.velocity.y = -PLAYER_SPEED;
-            player.animations.play('player_crawling');
-        } else if (cursors.down.isDown && player.body.y < 426) {
+            player.animations.play('player_walking');
+        } else if (cursors.down.isDown && player.body.y < 650) {
             player.body.velocity.y = PLAYER_SPEED;
-            player.animations.play('player_crawling');
+            player.animations.play('player_walking');
         }
     }
 }
@@ -119,7 +115,7 @@ function checkEndlessGeneration() {
     if (player.body.x > seamless_total * 1334) {
         seamless_total++;
         backgrounds.create(1334*seamless_total,0,'9_11_background');
-        tables.create(1334*seamless_total,200,'9_11_table');
+        moon_set.create(1334*seamless_total,200,'9_11_table');
         game.world.setBounds(0, 0, 1334*(seamless_total+1), 750);
         generateTrash();
     }
@@ -157,6 +153,19 @@ function secondTick() {
     }
 }
 
+function randomEntry() {
+    var randomizer = Math.floor(Math.random()*30);
+    if (randomizer == 0){
+        game.debug.text("left janitor");
+    } else if (randomizer == 0){
+        game.debug.text("right janitor");
+    } else if (randomizer == 0){
+        game.debug.text("left cameraman");
+    } else if (randomizer == 0){
+        game.debug.text("right cameraman");
+    }
+}
+
 function GameOver() {
     // TODO: show highscore table and enter highscore
 }
@@ -172,8 +181,8 @@ function fade() {
 
 function resetFade() {
     game.camera.resetFX();
-    player.body.x = PLAYER_START_X - 83.5;
-    player.body.y = PLAYER_START_Y - 57;
+    player.body.x = PLAYER_START_X - 50;
+    player.body.y = PLAYER_START_Y - 50;
     fading = false;
 }
 
