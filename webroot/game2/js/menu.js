2b8282d68a7c6aa2240de64fce84041ed1ca6f1b
ghwlanglois <ghwlanglois@gmail.com>
2016-10-06 12:40:03 -0400

    added menu js file

---
+++ webroot/game2/js/menu.js
@@ -0,0 +1,14 @@
+var game = new Phaser.Game(1334, 750, Phaser.AUTO, '',{ preload: preload, create: create, update: update });
+
+function preload() {
+	game.load.image('tag', 'assets/filename.png');
+	game.load.spritesheet('tag', 'assets/filename.png', width, height);
+}
+
+function create() {
+	// called once, when the game starts
+}
+
+function update() {
+	// called every frame
+}
